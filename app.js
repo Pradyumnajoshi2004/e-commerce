a74f2d8c94733ded1b55d7983e46955426dfcac4
Pradyumna joshi <148041767+Pradyumnajoshi2004@users.noreply.github.com>
2025-06-06 16:39:10 +0530

    Created Some Changes in app.js
    added cors package

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const userRoute = require("./Routes/userRoutes")
 const catogoryRoute = require("./Routes/catogoryRoute")
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
+
 require("dotenv/config")
 
 const app = express()
 
+
 // middleware
 app.use(express.json())
+app.use(cors())
 
 app.get("/",(req,res)=>{
     res.send("home")
